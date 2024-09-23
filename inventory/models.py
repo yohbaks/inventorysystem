2c00fbd79cf3b942710e72683529df68b1bac0c8
yohbaks <bobbyyu24@gmail.com>
2024-09-23 16:08:27 +0800

    latest

--- inventory/models.py
+++ inventory/models.py
@@ -26,16 +26,63 @@ class DESKTOPPACKAGE(models.Model):
     desktop_Processor = models.CharField(max_length=33)
     desktop_Memory = models.CharField(max_length=100)
     desktop_Drive = models.CharField(max_length=332)
-    desktop_Graphics = models.CharField(max_length=100)
-    desktop_Graphics_Size = models.CharField(max_length=100)
-    desktop_OS = models.CharField(max_length=100)
-    desktop_Office = models.CharField(max_length=100)
-    desktop_OS_keys = models.CharField(max_length=100)
-    desktop_Office_keys = models.CharField(max_length=100)
+
+    #Graphics
+    desktop_Graphics = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Graphics_Size = models.CharField(max_length=100, blank=True, null=True)
+    
+    desktop_OS = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Office = models.CharField(max_length=100, blank=True, null=True)
+    desktop_OS_keys = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Office_keys = models.CharField(max_length=100, blank=True, null=True)
     desktop_Image = models.ImageField(upload_to='equipment_images/', blank=True, null=True)  # Add this line
+    
+    # monitor
+    desktop_Monitor_SN = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Monitor_Brand = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Monitor_Model = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Monitor_Size = models.CharField(max_length=100, blank=True, null=True)
+    
+    # keyboard
+    desktop_Keyboard_SN = models.CharField(max_length=100, blank=True, null=True)
+    desktop_keyboard_Brand = models.CharField(max_length=100, blank=True, null=True)
+    desktop_keyboard_Model = models.CharField(max_length=100, blank=True, null=True)
+
+    # Mouse
+    desktop_Mouse_SN = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Mouse_Brand = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Mouse_Model = models.CharField(max_length=100, blank=True, null=True)
+
+    # UPS
+    desktop_UPS_SN = models.CharField(max_length=100, blank=True, null=True)
+    desktop_UPS_Brand = models.CharField(max_length=100, blank=True, null=True)
+    desktop_UPS_Model = models.CharField(max_length=100, blank=True, null=True)
+
+    # User details
+    desktop_Asset_owner = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Asset_designation = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Asset_section = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Enduser = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Enduser_designation = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Enduser_section = models.CharField(max_length=100, blank=True, null=True)
+
+    #Documents
+
+    desktop_PAR = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Propertyno = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Acquisition_Type = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Value = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Datereceived= models.CharField(max_length=100, blank=True, null=True)
+    desktop_Dateinspected = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Supplier = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Status = models.CharField(max_length=100, blank=True, null=True)
+    desktop_Computer_name = models.CharField(max_length=100, blank=True, null=True)
+
 
      # New field to track disposed status
-    is_disposed = models.BooleanField(default=False)  # Default is False (not disposed)
+
+    is_disposed = models.BooleanField(default=False)  # Field to indicate disposal status, Default is False (not disposed)
+    disposal_date = models.DateField(null=True, blank=True)  # Optional: Track the date of disposal
     
     def __str__(self):
         return 'IT EQUPMENT: ' + self.name 
--- inventory/urls.py
+++ inventory/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     path('add_equipment/', views.add_equipment_func, name='add_equipment_func_input'),
     path('success_add/', views.success_page, name='success_add_page'),
     path('<int:id>/', views.desktop_detailed_view, name='desktop_detailed_view'),  # Correct pattern for detailed view
+    path('disposed_desktop_list/', views.disposed_desktop_list, name='disposed_desktop_list'),  # List disposed desktops
+    path('dispose_desktop/<int:desktop_id>/', views.dispose_desktop, name='dispose_desktop'),  # Dispose a desktop
     path('desktop_all_detailed_view/', views.all_detailed_view, name='desktop_all_detailed_view'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
